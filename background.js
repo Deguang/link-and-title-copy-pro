7764bdae58c81071721bb8ec5ab9995acce7f892
duyu <duyu@duyudeMacBook-Pro.local>
2024-01-28 18:20:25 +0800

    [feature] add_context_menu

--- background.js
+++ background.js
@@ -13,3 +13,17 @@ chrome.commands.onCommand.addListener(function (command) {
     copyToClipboard();
   }
 });
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.contextMenus.create({
+    id: 'copyPageTitleAndUrl',
+    title: 'Copy Page Title and URL',
+    type: 'normal',
+    contexts: ['all']
+  })
+})
+
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+  if (info.menuItemId === 'copyPageTitleAndUrl') {
+    copyToClipboard()
+  }
+})
--- content.js
+++ content.js
@@ -5,6 +5,15 @@ function copyToClipboard() {
     navigator.clipboard.writeText(text).then(() => {
       // Send a message to the background script indicating that the copy operation is complete
       chrome.runtime.sendMessage({ type: 'copyComplete' });
+      // Show a notification to the user，indicating that the copy operation was successful
+      console.log(chrome.notifications)
+      chrome.notifications.create({
+        type: 'basic',
+        title: 'Basic Notification',
+        message: 'This is a Basic Notification',
+        iconUrl: 'icon.png'
+      })
+
     });
   }
   // This function adds the keyboard shortcut listener
